3eab8ccb173480989c118333358a37fa579bb24d
Glitch (sweeopy-bot) <none>
2020-08-21 04:53:09 +0000

    🐵🍱 Checkpoint ./server.js:21382115/2150

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const querystring = require('querystring');
 const discord = require('discord.js');
 const client = new discord.Client();
 
+const tags = [
+  { tag: '酸素と人数', url: 'https://gyazo.com/75dea51d415b74b6082d75fcdda8f08d' },
+  { tag: '作物の株数', url: 'https://gyazo.com/703af5dc05131d973eedf3f6280232f6' },
+  { tag: '作物の適温', url: 'https://gyazo.com/9bc68e5a03f78600a18e63c82dcbecd6' },
+  { tag: '気体の比重', url: 'https://gyazo.com/539b75221b72c0defc184ea84db0c7f9' },
+  { tag: '液体の水圧', url: 'https://gyazo.com/19017bac9164b8dd1160d2590187591c' },
+  { tag: '液体の比重', url: 'https://gyazo.com/7d5e226199facb9bb0e5b852d2210df1' },
+  { tag: '移動チューブ', url: 'https://gyazo.com/161b5e104c43e12aeef3ddc36cfa04fb' },
+  { tag: '液体クーラー', url: 'https://gyazo.com/06375d94aea03932592895cfc064dd1d' },
+]
+
 http.createServer(function(req, res){
   if (req.method == 'POST'){
     var data = "";
@@ -51,16 +62,17 @@ client.on('message', message =>{
     return;
   }
   if (message.content.match(/^\!tags/)){
-    let text = "```\n" +
-    "酸素と人数\n" +
-    "作物の株数\n" +
-    "作物の適温\n" +
-    "気体の比重\n" +
-    "液体の水圧\n" +
-    "液体の比重\n" +
-    "移動チューブ\n" +
-    "液体クーラー\n" +
-    "```";
+    const text = ["```", ...tags.map(o => o.tag) ,"```"].join("\n")
+    // let text = "```\n" +
+    // "酸素と人数\n" +
+    // "作物の株数\n" +
+    // "作物の適温\n" +
+    // "気体の比重\n" +
+    // "液体の水圧\n" +
+    // "液体の比重\n" +
+    // "移動チューブ\n" +
+    // "液体クーラー\n" +
+    // "```";
     sendMsg(message.channel.id, text);
     return;
   }
