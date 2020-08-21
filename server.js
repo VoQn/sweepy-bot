2a28234526c99fab1b4f3cb24654e62d23676ef1
Glitch (sweeopy-bot) <none>
2020-08-21 03:04:00 +0000

    Import of ExtEOi/DiscordBotBase

--- server.js
+++ server.js
@@ -1,34 +1,70 @@
-// server.js
-// where your node app starts
+const http = require('http');
+const querystring = require('querystring');
+const discord = require('discord.js');
+const client = new discord.Client();
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
+http.createServer(function(req, res){
+  if (req.method == 'POST'){
+    var data = "";
+    req.on('data', function(chunk){
+      data += chunk;
+    });
+    req.on('end', function(){
+      if(!data){
+        res.end("No post data");
+        return;
+      }
+      var dataObject = querystring.parse(data);
+      console.log("post:" + dataObject.type);
+      if(dataObject.type == "wake"){
+        console.log("Woke up in post");
+        res.end();
+        return;
+      }
+      res.end();
+    });
+  }
+  else if (req.method == 'GET'){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Discord Bot is active now\n');
+  }
+}).listen(3000);
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+client.on('ready', message =>{
+  console.log('Bot準備完了～');
+  client.user.setPresence({ game: { name: 'げーむ' } });
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+client.on('message', message =>{
+  if (message.author.id == client.user.id || message.author.bot){
+    return;
+  }
+  if(message.isMemberMentioned(client.user)){
+    sendReply(message, "呼びましたか？");
+    return;
+  }
+  if (message.content.match(/にゃ～ん|にゃーん/)){
+    let text = "にゃ～ん";
+    sendMsg(message.channel.id, text);
+    return;
+  }
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+if(process.env.DISCORD_BOT_TOKEN == undefined){
+ console.log('DISCORD_BOT_TOKENが設定されていません。');
+ process.exit(0);
+}
+
+client.login( process.env.DISCORD_BOT_TOKEN );
+
+function sendReply(message, text){
+  message.reply(text)
+    .then(console.log("リプライ送信: " + text))
+    .catch(console.error);
+}
+
+function sendMsg(channelId, text, option={}){
+  client.channels.get(channelId).send(text, option)
+    .then(console.log("メッセージ送信: " + text + JSON.stringify(option)))
+    .catch(console.error);
+}
