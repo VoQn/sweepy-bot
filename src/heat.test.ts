c202bb08db6a4c970471e7325e98aa93fcd39508
VoQn <voqn.tyrant@gmail.com>
2020-08-23 15:27:26 +0900

    Added calculation utility about heat

---
+++ src/heat.test.ts
@@ -0,0 +1,12 @@
+import { convertKelvinTemp } from "./heat";
+
+describe('heat', () => {
+  describe('convertKelvinTemp', () => {
+    it('convert 243.15K to -30℃', () => {
+      expect(convertKelvinTemp(243.15)).toEqual(-30);
+    });
+    it('convert 300.15K to 27℃', () => {
+      expect(convertKelvinTemp(300.15)).toEqual(27);
+    });
+  });
+});
---
+++ src/heat.ts
@@ -0,0 +1,22 @@
+export type TemparetureUnit = 'Kelvin' | 'Celsius' | 'Fahrenheit';
+
+const decimalShift = (x: number, p: number) => {
+  const xs = ("" + x).split("e");
+  return +(xs[0] + "e" + (xs[1] ? (+xs[1] + p) : p));
+};
+
+const round = (x: number, p: number) => {
+  return decimalShift(Math.round(decimalShift(x, p)), -p);
+}
+
+export const convertKelvinTemp = (temp: number, unit: TemparetureUnit = 'Celsius') => {
+  switch (unit) {
+    case 'Celsius':
+      return round(temp - 273.15, 2);
+    case 'Fahrenheit':
+      return round((9 / 5) * temp - 459.67, 2);
+    case 'Kelvin':
+    default:
+      return temp;
+  }
+};
