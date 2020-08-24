31390e6de857e96489fecde6096e4e93ba34277e
Glitch (sweepy-bot) <none>
2020-08-24 11:58:49 +0000

    🐗👩‍👩‍👦 Checkpoint ./src/commands/emojinate_command.ts:21383080/150

--- src/commands/emojinate_command.ts
+++ src/commands/emojinate_command.ts
@@ -3,9 +3,9 @@ import { emojinate } from './emojinate';
 
 class EmojinateComand implements CommandInterface{
     name = 'Emojinate';
-    pattern = /^\!emojinate\s+(?<arg>.+)$/;
-    help = `_出来るだけemojiに変換するよ_\n` +
-      '```!emojinate 今からliveやります!```';
+    pattern = /^\!emojinate\s+(?<arg>.+)$/ms;
+    help = ``_出来るだけ_ ${emojinate('emoji')} _に変換するよ_\n` +
+      '```!emojinate 今からliveやります!```'
     message(args: string | Object) {
       return '';
     }
