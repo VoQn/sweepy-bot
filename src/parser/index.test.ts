c428b6a44d53ccb73596cc69377cca5c7a097056
VoQn <voqn.tyrant@gmail.com>
2020-08-26 00:30:25 +0900

    Added command parser

---
+++ src/parser/index.test.ts
@@ -0,0 +1,25 @@
+import { parseCommand } from '.';
+
+describe('parseCommand', () => {
+  describe('with some arguments', () => {
+    it('!critter hatch', () => {
+      expect(parseCommand('!critter hatch')).toEqual({
+        command: 'critter',
+        args: 'hatch',
+      });
+    });
+  });
+  describe('without any argument', () => {
+    it('!cheatsheet', () => {
+      expect(parseCommand('!cheatsheet')).toEqual({
+        command: 'cheatsheet',
+        args: '',
+      });
+    });
+  });
+  describe('ignore any not command text', () => {
+    it('it\'s not command!', () => {
+      expect(parseCommand('it\'s not command!')).toBeNull();
+    });
+  });
+});
---
+++ src/parser/index.ts
@@ -0,0 +1,14 @@
+export type ParseResult = { command: string, args: string };
+
+export const parseCommand = (text: string): ParseResult => {
+  const withArgs = /^\!(?<command>\S+)[^\S\n]+(?<args>.+)$/;
+  const withoutArgs = /^\!(?<command>\S+)\s?$/;
+  if (withArgs.test(text)) {
+    return text.match(withArgs).groups as ParseResult;
+  }
+  if (withoutArgs.test(text)) {
+    const { command } = text.match(withoutArgs).groups;
+    return { command, args: '' };
+  }
+  return null;
+};
