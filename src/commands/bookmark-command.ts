8000bd937fd0f504734b3b49ed37dfeb6767b415
VoQn <voqn.tyrant@gmail.com>
2020-08-26 08:21:38 +0900

    decorate result of bookmark command

--- src/commands/bookmark-command.ts
+++ src/commands/bookmark-command.ts
@@ -1,5 +1,5 @@
 import { Command, CommandCategory } from './command';
-import { Client } from 'discord.js';
+import { Client, EmbedFieldData, MessageEmbedOptions } from 'discord.js';
 import { Response } from '../types';
 import bookmarks from '../../data/bookmark.json';
 
@@ -7,15 +7,39 @@ export const BookmarkCommand = Command.register({
   category: CommandCategory.ONI,
   name: 'Bookmark',
   help: {
-    summery: 'ONI のゲームプレイで便利なサイトを紹介するよ',
+    summery: 'ONI のゲームプレイで便利なサイトを紹介するよ\n' +
+      '```!bookmark```',
   },
-  exec: (_args: string, _client?: Client): Response => {
-    const links: string[] = [];
+  exec: (_args: string, client?: Client): Response => {
+    const color = 0xfcfc00;
+    const fields: EmbedFieldData[] = [];
+
     for (const siteInfo of Object.values(bookmarks)) {
-      links.push(`:link:[${siteInfo.name}](${siteInfo.url})`);
+      fields.push({
+        name: siteInfo.name,
+        value: `:link: _${siteInfo.url}_`,
+      });
+    }
+    const embed: MessageEmbedOptions = {
+      color,
+      title: ':bookmark: Bookmarks',
+      fields,
+      timestamp: new Date(),
+    };
+    if (client) {
+      const sweepyIcon = client.user.avatarURL();
+      embed.author = {
+        name: 'Sweepy Not',
+        iconURL: sweepyIcon,
+      };
+      embed.footer = {
+        text: 'Sweepy Bot',
+        iconURL: sweepyIcon,
+      };
     }
     return {
-      content: links.join('\n'),
+      content: ':bookmark: _ブックマークしているサイトだよ_',
+      options: { embed },
     };
   },
 });
