aaf4a7618b798129754a956c848e8b9589d86997
Takumi Abe <abe@engraphia.com>
2020-08-27 20:58:30 +0900

    delete AnswerTalker

--- src/commands/cheatsheet-command.ts
+++ src/commands/cheatsheet-command.ts
@@ -1,13 +1,57 @@
-import { AnswerTalker } from '../answer-talker';
 import { Response } from '../types';
 import { Command, CommandCategory } from './command';
-import cheatsheets from '../data/cheatsheet.json';
 
-const answerTalker = new AnswerTalker(
-  Object.values(cheatsheets),
-  'name',
-  'url'
-);
+import cheatsheet_data from '../data/cheatsheet.json';
+const cheatsheets = Object.values(cheatsheet_data);
+type Cheatsheet = { name: string, url: string }
+
+function getKeywords() {
+  return ['```', ...cheatsheets.map((o) => o.name), '```'].join('\n')
+}
+
+const defaultAnswer = ':thinking: なんのこと？';
+
+function getAnswer(arg: string): string {
+  // keywordと完全一致検索する
+  const exact: Cheatsheet = exact_match(arg);
+  if (exact !== null) {
+    return exact.url;
+  }
+
+  // keywordと部分一致検索する
+  const choice: Array<Cheatsheet> = partial_match(arg);
+  if (choice.length === 1) {
+    const name = choice[0].name;
+    const url = choice[0].url;
+
+    return [
+      ':bulb: もしかして、これ？',
+      '```',
+      name,
+      '```',
+      url,
+    ].join('\n');
+  }
+
+  if (choice.length > 1) {
+    return [
+      ':thinking: 複数あるよ。聞き直してね。',
+      '```',
+      ...choice.map((o) => o.name),
+      '```',
+    ].join('\n');
+  }
+
+  return defaultAnswer;
+}
+
+function exact_match(arg: string): Cheatsheet | null {
+  return cheatsheets.find((o) => o.name === arg);
+}
+
+function partial_match(arg: string): Array < Cheatsheet > {
+  return cheatsheets.filter((o) => o.name.includes(arg));
+}
 
 export const CheatsheetCommand: Command = Command.register({
   category: CommandCategory.ONI,
@@ -26,13 +70,16 @@ export const CheatsheetCommand: Command = Command.register({
       '```!cheatsheet 比重```',
     ].join('\n'),
   },
+
   exec: (args: string): Response => {
-    if (!args) {
+    if (!args || args.length == 0) {
       // チートシート一覧
-      return { content: answerTalker.getKeywords() };
+      return { content: getKeywords() };
+    } else if (args.length < 2) {
+      return { content: ':thinking: もうちょっとヒントちょうだい (2文字以上欲しがっています)' };
     } else {
       // チートシートの返答
-      return { content: answerTalker.getAnswer(args) };
+      return { content: getAnswer(args) };
     }
-  },
+  }
 });
