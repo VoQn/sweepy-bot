babe0af5d4caf73cf6aaad8bca8c6da2c5a28ff2
Takumi Abe <abe@engraphia.com>
2020-09-05 17:16:49 +0900

    Element-Commandを実装

---
+++ packages/sweepy-dock/src/element/elements.ts
@@ -0,0 +1,4 @@
+import elementsData from '../data/elements.json';
+import { Element } from './element-info';
+
+export const elements: Array<Element> = elementsData.elements as Array<Element>;
---
+++ packages/sweepy-dock/src/element/index.ts
@@ -0,0 +1,3 @@
+export * from './element-info';
+export * from './elements';
+export * from './element-properties';
